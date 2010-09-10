ad791dc08aa03fb8e7073532c2f9b1ef895041e5
massdosage <massdosage@gmail.com>
2010-09-10 16:34:44 +0000

    - http://code.google.com/p/citrine-scheduler/issues/detail?id=28 (notifyOnFailure is null in default data)

--- src/sql/base_data.sql
+++ src/sql/base_data.sql
@@ -1,8 +1,8 @@
-insert into tasks (command,                                        description,                                      enabled, beanName,     name,               groupName, priority,timerSchedule,  version, errorIfRunning, stopOnError, notifyOnSuccess) 
+insert into tasks (command,                                        description,                                      enabled, beanName,     name,               groupName, priority,timerSchedule,  version, errorIfRunning, stopOnError, notifyOnSuccess, notifyOnFailure) 
           values ('clear_log_files 30','Cleans up Citrine log files older than N days.
-By default N is 30, this can be set as the first argument to the job.', true,    'adminJob', 'Log File Cleaner', 'admin',   0,        '0 0 3 * * ?', 0,       true,           true,        false);
+By default N is 30, this can be set as the first argument to the job.', true,    'adminJob', 'Log File Cleaner', 'admin',   0,        '0 0 3 * * ?', 0,       true,           true,        false,   true);
 
-insert into tasks (command,          description,                                            enabled, beanName,     name,               groupName, priority,timerSchedule,  version, errorIfRunning, stopOnError, notifyOnSuccess) 
+insert into tasks (command,          description,                                            enabled, beanName,     name,               groupName, priority,timerSchedule,  version, errorIfRunning, stopOnError, notifyOnSuccess, notifyOnFailure) 
           values ('clear_task_runs 30', 'Cleans up Task Runs in the database older than N days.
-By default N is 30, this can be set as the first argument to the job.', true,    'adminJob', 'Task Run Cleaner', 'admin',   0,        '0 0 3 * * ?', 0,       true,           true,        false); 
+By default N is 30, this can be set as the first argument to the job.', true,    'adminJob', 'Task Run Cleaner', 'admin',   0,        '0 0 3 * * ?', 0,       true,           true,        false,   true); 
  
